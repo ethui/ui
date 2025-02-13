539c74c57d8db78bf893861082aefd2f324b30b7
Miguel Palhas <mpalhas@gmail.com>
2025-02-13 15:23:01 +0000

    ButtonWithDropdown

---
+++ stories/ButtonWithDropdown.stories.tsx
@@ -0,0 +1,39 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+
+import { ButtonWithDropdown } from "../components/button-with-dropdown.js";
+import { Button } from "../components/shadcn/button.js";
+import React from "react";
+
+const meta = {
+  title: "Components/ButtonWithDropdown",
+  component: ButtonWithDropdown,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    variant: { control: "text" },
+    children: { control: "text" },
+    options: { control: undefined },
+  },
+  args: { onClick: fn() },
+} satisfies Meta<typeof ButtonWithDropdown>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    variant: "default",
+    children: "macOS",
+    options: [
+      <Button key="macOS" variant="ghost">
+        macOS
+      </Button>,
+      <Button key="Linux" variant="ghost">
+        Linux
+      </Button>,
+    ],
+  },
+};
