5615f1f12356a7d1b9f0a7a250841bfca05efd90
Miguel Palhas <mpalhas@gmail.com>
2025-02-16 19:22:57 +0000

    wip

--- components/infinite-scroll.tsx
+++ components/infinite-scroll.tsx
@@ -1,62 +1,85 @@
-// Initial draft from v0:
-// https://v0.dev/chat/load-more-on-scroll-K3f4RYuezLB
+// adapted from https://shadcnui-expansions.typeart.cc/docs/infinite-scroll
 
-import { useState, useEffect, useRef } from "react";
-import { useInView } from "../hooks/use-in-view";
-import { LoaderCircle } from "lucide-react";
+import * as React from "react";
+import { useCallback, useRef } from "react";
 
-interface Item {
-  id: number;
-  title: string;
+interface InfiniteScrollProps {
+  isLoading: boolean;
+  hasMore: boolean;
+  next: () => unknown;
+  threshold?: number;
+  root?: Element | Document | null;
+  rootMargin?: string;
+  reverse?: boolean;
+  children?: React.ReactNode;
 }
 
-interface Page<T> {
-  items: T[];
-  finished: boolean;
-}
+export function InfiniteScroll({
+  isLoading,
+  hasMore,
+  next,
+  threshold = 0.5,
+  root = null,
+  rootMargin = "0px",
+  reverse,
+  children,
+}: InfiniteScrollProps) {
+  const observer = useRef<IntersectionObserver>(null);
+  // This callback ref will be called when it is dispatched to an element or detached from an element,
+  // or when the callback function changes.
+  const observerRef = useCallback(
+    (element: HTMLElement | null) => {
+      let safeThreshold = threshold;
+      if (threshold < 0 || threshold > 1) {
+        console.warn(
+          "threshold should be between 0 and 1. You are exceed the range. will use default value: 1",
+        );
+        safeThreshold = 1;
+      }
+      // When isLoading is true, this callback will do nothing.
+      // It means that the next function will never be called.
+      // It is safe because the intersection observer has disconnected the previous element.
+      if (isLoading) return;
 
-interface Props {
-  children: (item: Item) => React.ReactNode;
-  onLoadMoreEnd: (items: Item[]) => Promise<Page<Item>>;
-}
+      if (observer.current) observer.current.disconnect();
+      if (!element) return;
 
-export function InfiniteScroll({ children, onLoadMoreEnd }: Props) {
-  const [items, setItems] = useState<Item[]>([]);
-  const [loading, setLoading] = useState(false);
-  const endRef = useRef(null);
-  const containerRef = useRef(null);
-  const endInView = useInView({ ref: endRef, parent: containerRef });
+      // Create a new IntersectionObserver instance because hasMore or next may be changed.
+      observer.current = new IntersectionObserver(
+        (entries) => {
+          if (entries[0].isIntersecting && hasMore) {
+            next();
+          }
+        },
+        { threshold: safeThreshold, root, rootMargin },
+      );
+      observer.current.observe(element);
+    },
+    [hasMore, isLoading, next, threshold, root, rootMargin],
+  );
 
-  // biome-ignore lint/correctness/useExhaustiveDependencies(items): would cause a race-condition and multiple loads
-  useEffect(() => {
-    if (endInView && !loading) {
-      console.log(endInView);
-      setLoading(true);
-      onLoadMoreEnd(items).then(({ items, finished }) => {
-        setItems((prevItems) => [...prevItems, ...items]);
-        // workaround to ensure we don't load two pages accidentally due to race conditions
-        // enforces a delay of at least 300ms after the last content has been loaded
-        setTimeout(() => {
-          setLoading(false);
-        }, 200);
-      });
-    }
-  }, [endInView, loading, onLoadMoreEnd]);
+  const flattenChildren = React.useMemo(
+    () => React.Children.toArray(children),
+    [children],
+  );
 
   return (
-    <div
-      className="h-[800px] w-full overflow-y-scroll rounded-lg border border-gray-200 p-4"
-      ref={containerRef}
-    >
-      <h2 className="mb-4 font-bold text-2xl">Infinite Scroll Demo</h2>
-      {items.map(children)}
-      <div ref={endRef} className="flex justify-center py-4">
-        {loading ? (
-          <LoaderCircle className="animate-spin" />
-        ) : (
-          <p className="text-gray-500">Load more</p>
-        )}
-      </div>
-    </div>
+    <>
+      {flattenChildren.map((child, index) => {
+        if (!React.isValidElement(child)) {
+          process.env.NODE_ENV === "development" &&
+            console.warn("You should use a valid element with InfiniteScroll");
+          return child;
+        }
+
+        const isObserveTarget = reverse
+          ? index === 0
+          : index === flattenChildren.length - 1;
+        console.log("is", isObserveTarget);
+        const ref = isObserveTarget ? observerRef : null;
+        // @ts-ignore ignore ref type
+        return React.cloneElement(child, { ref });
+      })}
+    </>
   );
 }
--- stories/infinite-scroll.stories.tsx
+++ stories/infinite-scroll.stories.tsx
@@ -1,3 +1,4 @@
+import { LoaderCircle } from "lucide-react";
 import type { Meta, StoryObj } from "@storybook/react";
 
 import { InfiniteScroll } from "../components/infinite-scroll.js";
@@ -16,29 +17,61 @@ const meta: Meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
+interface Item {
+  id: number;
+  title: string;
+}
+
+const Item = ({ item }: { item: Item }) => {
+  return (
+    <div className="mb-4 p-4">
+      <h3 className="font-semibold text-lg">{item.title}</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
+  );
+};
+
 export const Default: Story = {
   render: () => {
+    const [loading, setLoading] = React.useState(false);
+    const [hasMore, setHasMore] = React.useState(true);
+    const [items, setItems] = React.useState<Item[]>([]);
+    const listRef = React.useRef<HTMLDivElement>(null);
+
+    const next = async () => {
+      setLoading(true);
+
+      const newItems = Array.from({ length: 10 }, (_, i) => ({
+        id: items.length + i + 1,
+        title: `Item ${items.length + i + 1}`,
+      }));
+
+      setItems((prev) => [...prev, ...newItems]);
+
+      // Usually your response will tell you if there is no more data.
+      if (items.length + newItems.length >= 100) {
+        setHasMore(false);
+      }
+      setLoading(false);
+    };
+
     return (
-      <InfiniteScroll
-        onLoadMoreEnd={async (items) => {
-          return {
-            items: Array.from({ length: 10 }, (_, i) => ({
-              id: items.length + i + 1,
-              title: `Item ${items.length + i + 1}`,
-            })),
-            finished: items.length >= 100,
-          };
-        }}
-      >
-        {(item) => (
-          <div key={item.id} className="mb-4 rounded-lg bg-white p-4 shadow">
-            <h3 className="font-semibold text-lg">{item.title}</h3>
-            <p className="text-gray-600">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </p>
-          </div>
-        )}
-      </InfiniteScroll>
+      <div className="max-h-[600px] w-full overflow-y-auto px-10" ref={listRef}>
+        <div className="flex w-full flex-col items-center gap-3">
+          {items.map((item) => (
+            <Item key={item.id} item={item} />
+          ))}
+          <InfiniteScroll
+            hasMore={hasMore}
+            isLoading={loading}
+            next={next}
+            threshold={0.5}
+            root={listRef.current}
+          >
+            {hasMore && <LoaderCircle className="animate-spin" />}
+          </InfiniteScroll>
+        </div>
+      </div>
     );
   },
 };
