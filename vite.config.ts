f9b803197f273c10afdadb860da03e0069028e77
Miguel Palhas <mpalhas@gmail.com>
2025-02-18 16:15:54 +0000

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths({ parseNative: true })],
+  plugins: [react(), tailwindcss(), tsconfigPaths({ parseNative: true })],
 });
