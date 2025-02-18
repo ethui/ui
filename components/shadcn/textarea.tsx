afa56599769c37f70971c83e05361015b5c8b43a
Miguel Palhas <mpalhas@gmail.com>
2025-02-18 18:11:13 +0000

    wip

--- components/shadcn/textarea.tsx
+++ components/shadcn/textarea.tsx
@@ -2,7 +2,7 @@ import { cn } from "../../lib/utils.js";
 
 function Textarea({ className, ...props }: React.ComponentProps<"textarea">) {
   return (
-    <Textarea
+    <textarea
       className={cn(
         "flex min-h-[60px] w-full border border-input bg-transparent px-3 py-2 text-base placeholder:text-muted-foreground focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
         className,
