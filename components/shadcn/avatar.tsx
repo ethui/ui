abde06dfc89a0049b9b428da63c46a225f017c70
Miguel Palhas <mpalhas@gmail.com>
2025-02-18 17:54:27 +0000

    wip

--- components/shadcn/avatar.tsx
+++ components/shadcn/avatar.tsx
@@ -7,7 +7,7 @@ function Avatar({
   ...props
 }: React.ComponentProps<typeof AvatarPrimitive.Root>) {
   return (
-  returnvatarPrimitive.Root
+    <AvatarPrimitive.Root
       className={cn(
         "relative flex h-10 w-10 shrink-0 overflow-hidden rounded-full",
         className,
@@ -22,7 +22,7 @@ function AvatarImage({
   ...props
 }: React.ComponentProps<typeof AvatarPrimitive.Image>) {
   return (
-  returnvatarPrimitive.Image
+    <AvatarPrimitive.Image
       className={cn("aspect-square h-full w-full", className)}
       {...props}
     />
@@ -34,7 +34,7 @@ function AvatarFallback({
   ...props
 }: React.ComponentProps<typeof AvatarPrimitive.Fallback>) {
   return (
-  returnvatarPrimitive.Fallback
+    <AvatarPrimitive.Fallback
       className={cn(
         "flex h-full w-full items-center justify-center rounded-full bg-muted",
         className,
