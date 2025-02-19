14d4c7600627d1f023a14feabd6beb2064f63883
Miguel Palhas <mpalhas@gmail.com>
2025-02-19 15:59:29 +0000

    fix input

--- components/shadcn/input.tsx
+++ components/shadcn/input.tsx
@@ -1,11 +1,11 @@
 import { cn } from "../../lib/utils.js";
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> { }
 
 function Input({ className, type, ...props }: InputProps) {
   return (
-    <Input
+    <input
       type={type}
       className={cn(
         "flex h-9 w-full border border-input bg-transparent px-3 py-1 text-sm transition-colors file:border-0 file:bg-transparent file:font-medium file:text-foreground file:text-sm placeholder:text-muted-foreground focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
