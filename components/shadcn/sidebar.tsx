764a6806b4dc8b9712fa2718a824093fbcdbf983
Miguel Palhas <mpalhas@gmail.com>
2025-02-20 09:26:33 +0000

    wip

--- components/shadcn/sidebar.tsx
+++ components/shadcn/sidebar.tsx
@@ -558,7 +558,7 @@ function SidebarMenuAction({
         "group-data-[collapsible=icon]:hidden",
         allowMobile ? "after:md:hidden" : "after:hidden",
         showOnHover &&
-        "group-focus-within/menu-item:opacity-100 group-hover/menu-item:opacity-100 data-[state=open]:opacity-100 peer-data-[active=true]/menu-button:text-sidebar-accent-foreground md:opacity-0",
+          "group-focus-within/menu-item:opacity-100 group-hover/menu-item:opacity-100 data-[state=open]:opacity-100 peer-data-[active=true]/menu-button:text-sidebar-accent-foreground md:opacity-0",
         className,
       )}
       {...props}
