ed433a36bfabbf78f4a7e8c3c6c33e7589925f85
Miguel Palhas <mpalhas@gmail.com>
2025-02-20 14:33:08 +0000

    fix Switch

--- components/shadcn/switch.tsx
+++ components/shadcn/switch.tsx
@@ -6,19 +6,21 @@ function Switch({
   className,
   ...props
 }: React.ComponentProps<typeof SwitchPrimitives.Root>) {
-  <SwitchPrimitives.Root
-    className={cn(
-      "peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center border-2 border-transparent shadow-xs transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input",
-      className,
-    )}
-    {...props}
-  >
-    <SwitchPrimitives.Thumb
+  return (
+    <SwitchPrimitives.Root
       className={cn(
-        "pointer-events-none block h-4 w-4 bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0",
+        "peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center border-2 border-transparent shadow-xs transition-colors focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input",
+        className,
       )}
-    />
-  </SwitchPrimitives.Root>;
+      {...props}
+    >
+      <SwitchPrimitives.Thumb
+        className={cn(
+          "pointer-events-none block h-4 w-4 bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0",
+        )}
+      />
+    </SwitchPrimitives.Root>
+  );
 }
 
 export { Switch };
