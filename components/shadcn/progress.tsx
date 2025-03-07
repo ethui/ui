64f2950097af5783c5f03936aadccdf5b7c0f977
Miguel Palhas <mpalhas@gmail.com>
2025-03-07 15:42:38 +0000

    Adding Progress

---
+++ components/shadcn/progress.tsx
@@ -0,0 +1,28 @@
+import * as ProgressPrimitive from "@radix-ui/react-progress";
+
+import { cn } from "../../lib/utils.js";
+
+function Progress({
+  className,
+  value,
+  ref,
+  ...props
+}: React.ComponentProps<typeof ProgressPrimitive.Root>) {
+  return (
+    <ProgressPrimitive.Root
+      ref={ref}
+      className={cn(
+        "relative h-2 w-full overflow-hidden bg-primary/20",
+        className,
+      )}
+      {...props}
+    >
+      <ProgressPrimitive.Indicator
+        className="h-full w-full flex-1 bg-primary transition-all"
+        style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+      />
+    </ProgressPrimitive.Root>
+  );
+}
+
+export { Progress };
