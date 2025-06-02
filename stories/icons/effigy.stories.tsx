06727d01e7e08e12f49f262d84fc495a7f316c13
Miguel Palhas <mpalhas@gmail.com>
2025-06-02 18:30:45 +0100

    Upgrade to storybook 9 (#93)

--- stories/icons/effigy.stories.tsx
+++ stories/icons/effigy.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react-vite";
 
 import { EffigyIcon } from "../../components/icons/effigy.js";
 
