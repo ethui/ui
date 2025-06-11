374e347bbbb19ccc1703e175b010479b9875619d
Miguel Palhas <mpalhas@gmail.com>
2025-06-11 12:44:21 +0100

    wip

--- components/icons/chain.tsx
+++ components/icons/chain.tsx
@@ -1,5 +1,6 @@
 import { clsx } from "clsx";
 
+import { cn } from "lib/utils";
 import mainnet from "../../images/chains/1.webp";
 import optimism from "../../images/chains/10.webp";
 import polygon from "../../images/chains/137.webp";
@@ -16,7 +17,6 @@ import scroll from "../../images/chains/534352.webp";
 import sepolia from "../../images/chains/11155111.webp";
 import optimismSepolia from "../../images/chains/11155420.webp";
 import unknown from "../../images/chains/unknown.webp";
-import { cn } from "lib/utils";
 
 export interface IconChainProps {
   chainId: number;
