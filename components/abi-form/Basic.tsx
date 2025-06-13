69ec2533edc3305c939c12f09e2c7d3c979f9ae0
joaocosta9 <johnydacosta@hotmail.com>
2025-06-13 16:57:37 +0100

    add abi item form

---
+++ components/abi-form/Basic.tsx
@@ -0,0 +1,33 @@
+import { useCallback, useState } from "react";
+
+import type { InnerProps } from "./AbiInput";
+
+import { parse } from "@ethui/abiparse";
+import { Debug, stringify } from "lib/utils";
+import { Input } from "components/shadcn/input";
+
+export type BasicProps = Omit<InnerProps, "depth" | "type" | "label">;
+export function Basic({ name, defaultValue, onChange, debug }: BasicProps) {
+  const [value, setValue] = useState(defaultValue);
+
+  const handleChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const value = parse(e.target.value);
+      setValue(value);
+      onChange(value);
+    },
+    [onChange],
+  );
+
+  return (
+    <div>
+      <Input
+        type="text"
+        name={name}
+        onChange={handleChange}
+        defaultValue={defaultValue && stringify(defaultValue, 0)}
+      />
+      {debug && <Debug value={value} />}
+    </div>
+  );
+}
