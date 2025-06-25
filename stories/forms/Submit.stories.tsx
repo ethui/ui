1fdceccaf416c4f0c987698c5c10e18818561a52
Miguel Palhas <mpalhas@gmail.com>
2025-06-25 11:46:34 +0100

    wip

---
+++ stories/forms/Submit.stories.tsx
@@ -0,0 +1,106 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Form } from "../../components/form.js";
+
+const meta = {
+  title: "Components/Form/Submit",
+  component: Form.Submit,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  decorators: [
+    (Story) => (
+      <div className="w-md">
+        <Story />
+      </div>
+    ),
+  ],
+} satisfies Meta<typeof Form.Submit>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const WithRequiredInput: Story = {
+  decorators: [
+    (Story) => {
+      const schema = z.object({
+        required: z.string().min(1),
+      });
+
+      const form = useForm({
+        resolver: zodResolver(schema),
+      });
+
+      const onSubmit = async () => {
+        console.log("submitting");
+        await new Promise((resolve) => setTimeout(resolve, 500));
+        console.log("submitted");
+        form.reset({}, { keepValues: true });
+      };
+
+      return (
+        <Form form={form} onSubmit={onSubmit}>
+          <Form.Text
+            name="required"
+            label="Required field"
+            className="w-full"
+          />
+          <div className="flex gap-2">
+            <Story />
+          </div>
+        </Form>
+      );
+    },
+  ],
+
+  args: {
+    name: "default",
+    label: "Save",
+    className: "w-full",
+  },
+};
+
+export const WithOptionalInput: Story = {
+  decorators: [
+    (Story) => {
+      const schema = z.object({
+        optional: z.string().optional(),
+      });
+
+      const form = useForm({
+        resolver: zodResolver(schema),
+      });
+
+      const onSubmit = async () => {
+        console.log("submitting");
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        console.log("submitted");
+        form.reset({}, { keepValues: true });
+      };
+
+      return (
+        <Form form={form} onSubmit={onSubmit}>
+          <Form.Text
+            name="optional"
+            label="Optional field"
+            className="w-full"
+          />
+          <div className="flex gap-2">
+            <Story />
+          </div>
+        </Form>
+      );
+    },
+  ],
+
+  args: {
+    name: "default",
+    label: "Default text input",
+    className: "w-full",
+  },
+};
