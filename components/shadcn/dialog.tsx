bc1349708117e5389cb18dd0aae2524c0ff04a33
joaocosta9 <joaomcosta6@gmail.com>
2025-09-30 11:17:12 +0100

    Add Command component story (#134)
    * add command
    
    * add command

--- components/shadcn/dialog.tsx
+++ components/shadcn/dialog.tsx
@@ -26,15 +26,35 @@ function DialogOverlay({
   );
 }
 
+interface DialogContentProps
+  extends React.ComponentProps<typeof DialogPrimitive.Content> {
+  position?: "top" | "center" | "bottom";
+  containerClassName?: string;
+}
+
 function DialogContent({
   className,
   children,
+  position = "center",
+  containerClassName,
   ...props
-}: React.ComponentProps<typeof DialogPrimitive.Content>) {
+}: DialogContentProps) {
+  const positionClasses = {
+    top: "items-start pt-[5vh]",
+    center: "items-center",
+    bottom: "items-end pb-[5vh]",
+  };
+
   return (
     <DialogPortal>
       <DialogOverlay />
-      <div className="fixed inset-0 z-50 flex items-center justify-center">
+      <div
+        className={cn(
+          "fixed inset-0 z-50 flex justify-center",
+          positionClasses[position],
+          containerClassName,
+        )}
+      >
         <DialogPrimitive.Content
           className={cn(
             "data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-100 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-top-[-2%] data-[state=open]:slide-in-from-top-[-2%] relative grid w-full max-w-lg translate-y-[0%] gap-4 border bg-background p-6 duration-200 data-[state=closed]:animate-out data-[state=open]:animate-in",
